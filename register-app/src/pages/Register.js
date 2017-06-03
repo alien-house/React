02d75c53c653f2931ff6acf494e350ba87ffa381
alien-house <info@alien-house.com>
2017-06-02 21:32:11 -0700

    add Authentication

--- register-app/src/pages/Register.js
+++ register-app/src/pages/Register.js
@@ -11,6 +11,10 @@ firebase.initializeApp(config);
 export default class Register extends React.Component {
 	constructor(){
 		super();
+		// this.state = {value: 'haiteru'};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 		this.state = {
 			speed: 10
 		};
@@ -18,18 +22,64 @@ export default class Register extends React.Component {
 	// only once
 	componentDidMount(){
 
-		const rootRef = firebase.database().ref().child('user');
-		const userRef = rootRef.child('speed');
-		userRef.on('value', snap => {
-		console.log(snap);
-			this.setState({
-				speed: snap.val()
-			});
-		});
+		// const rootRef = firebase.database().ref().child('user');
+		// const userRef = rootRef.child('speed');
+		// userRef.on('value', snap => {
+		// // console.log(snap);
+		// 	this.setState({
+		// 		speed: snap.val()
+		// 	});
+		// });
 	}
 
-	handleClick(i) {
+	// handleClick(i) {
 		
+	// }
+
+	handleChange(event) {
+    	const target = event.target;
+    	const value = target.value;
+    	const name = target.name;
+		this.setState({
+			[name]: value
+		});
+		// console.log(this.state);
+		// console.log(this.state.email);
+		// this.setState({value: event.target.value});
+	}
+
+	handleSubmit(event) {
+		// console.log('An essay was submitted: ' + this.state.value);
+		var email = this.state.email;
+		var password = this.state.password;
+		if (email === undefined || password === undefined){
+			alert('.');
+			return;
+		}
+		if (email.length < 4) {
+			alert('Please enter an email addressed.');
+			return;
+		}
+		if (password.length < 4) {
+			alert('Please enter a password.');
+			return;
+		}
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// [START_EXCLUDE]
+			if (errorCode === 'auth/weak-password') {
+				alert('The password is too weak.');
+			} else {
+				alert(errorMessage);
+			}
+			console.log(error);
+			// [END_EXCLUDE]
+		});
+
+		event.preventDefault();
+
 	}
 
 	render() {
@@ -42,13 +92,15 @@ export default class Register extends React.Component {
 			<div>
 			<h1>Register</h1>
 			<p>{this.state.speed}</p>
+			<form onSubmit={this.handleSubmit}>
 			<dl>
 				<dt>id</dt>
-				<dd><input /></dd>
+				<dd><input type="text" name="email" value={this.state.value} onChange={this.handleChange} /></dd>
 				<dt>pass</dt>
-				<dd><input /></dd>
+				<dd><input type="text" name="password" value={this.state.value} onChange={this.handleChange} /></dd>
 			</dl>
 			<button>登録</button>
+			</form>
 			</div>
 		);
 	}
