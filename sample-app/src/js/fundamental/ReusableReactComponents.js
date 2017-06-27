4930649c9c16be72da083c47a2292fa6ff7a95ac
alien-house <info@alien-house.com>
2017-06-27 15:34:36 -0700

    ReusableReactComponents

--- sample-app/src/js/fundamental/ReusableReactComponents.js
+++ sample-app/src/js/fundamental/ReusableReactComponents.js
@@ -5,27 +5,70 @@ export default class ReusableReactComponents extends React.Component {
 	constructor(){
 		super();
 		this.state={
-			red:0,
-			green:0,
-			blue:0
+			red:0
 		}
 		this.update = this.update.bind(this)
 	}
 	update(e){
 		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value,
+			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
 		})
 	}
 	render(){
 		return(
 			<div>
-				<Slider ref="red" update={this.update} />
+				<NumInput
+				ref="red"
+				min={0}
+				max={255}
+				step={1}
+				val={+this.state.red}
+				label="Red"
+				update={this.update} />
+				{this.state.red}
+			</div>
+		);
+	}
+}
+
+class NumInput extends React.Component{
+	render(){
+		let label = this.props.label !== '' ?
+		<label>{this.props.label} - {this.props.val}</label> : ''
+		return(
+			<div>
+				<input ref="inp"
+				type={this.props.type}
+				min={this.props.min}
+				max={this.props.max}
+				step={this.props.step}
+				defaultValue={this.props.val}
+				onChange={this.props.update} />
+				{label}
 			</div>
 		)
 	}
 }
+
+NumInput.propTypes = {
+	min: React.PropTypes.number,
+	max: React.PropTypes.number,
+	step: React.PropTypes.number,
+	val: React.PropTypes.number,
+	label: React.PropTypes.string,
+	update: React.PropTypes.func.isRequired,
+	type: React.PropTypes.oneOf(['number','range'])
+}
+NumInput.defaultProps = {
+	min: 0,
+	max: 0,
+	step: 1,
+	val: 0,
+	label: '',
+	type: 'range'
+}
+
+
 
 
 
