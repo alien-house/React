d4b92f972a249dc80549f1bf8bc2f75167d2f3df
sin <info@alien-house.com>
2017-07-27 01:03:58 -0700

    Delete config.js

--- kitten/src/utils/config.js
+++
@@ -1,7 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyCz4_2OhpXg6TPdYWRXnt330dRyD5q1jFA",
-  authDomain: "programming-473ea.firebaseapp.com",
-  databaseURL: "https://programming-473ea.firebaseio.com",
-  storageBucket: "programming-473ea.appspot.com",
-};
