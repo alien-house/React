9bf9482b5e0fe7fc426c7505ad0852cbfb4de786
alien-house <info@alien-house.com>
2017-08-13 17:07:58 -0700

    learn more redux

---
+++ react-redux/src/reducers/userReducer.js
@@ -0,0 +1,23 @@
+const userReducer = (state = {
+  name: "Max",
+  age: 27
+}, action) => {
+  switch (action.type){
+    case "SET_NAME":
+      state = {
+        ...state,
+        name: action.payload
+      };
+      // state.lastValues.push(action.payload);
+      break;
+    case "SET_AGE":
+        state = {
+          ...state,
+          age: action.payload
+        };
+        // state.lastValues.push(action.payload);
+      break;
+  }
+  return state;
+};
+export default userReducer;
